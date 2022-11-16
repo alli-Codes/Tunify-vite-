ee3095e681db8b3f0d338f6594d3580fca484ba8
alli-Codes <allisonevans081@gmail.com>
2022-11-16 15:14:53 +0100

    adding a custom color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#00D38B",
+    },
+    // extend: {
+
+    // },
   },
   plugins: [],
 };
