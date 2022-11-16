fb57d21d9ea3aad28fba8358eab729cade48ec49
alli-Codes <allisonevans081@gmail.com>
2022-11-16 14:27:45 +0100

    Creating a default component page

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import Home from "./components/Default.vue";
+
+export default {
+  name: "App",
+  components: {
+    Default,
+  },
+};
+</script>
+
+<template>
+  <Default />
+</template>
