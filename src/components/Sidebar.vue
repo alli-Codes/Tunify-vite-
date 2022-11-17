b952ed5292f016eec45132863bc009e0903bbe82
alli-Codes <allisonevans081@gmail.com>
2022-11-17 11:07:53 +0100

    Adding the links

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,28 @@
 <script></script>
 
 <template>
-  <div class="h-full w-[20rem] bg-[#FFF8F8] absolute top-0"></div>
+  <div class="h-full w-[20rem] bg-[#FFF8F8] absolute top-0">
+    <div class="bg-red-400 flex flex-col relative top-20">
+      <section>
+        <p>BROWSER</p>
+        <nav class="flex flex-col">
+          <a href="#">Home</a>
+          <a href="#">Explore</a>
+          <a href="#">Songs</a>
+          <a href="#">Artists</a>
+          <a href="#">Albums</a>
+          <a href="#">Radio</a>
+        </nav>
+      </section>
+
+      <section>
+        <p>YOUR LIBRARY</p>
+        <nav class="flex flex-col">
+          <a href="#">Recently Played</a>
+          <a href="#">Favourites</a>
+          <a href="#">Playlists</a>
+        </nav>
+      </section>
+    </div>
+  </div>
 </template>
