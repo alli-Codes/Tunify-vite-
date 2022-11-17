5ab4db30ed40021a2e4203148975fc04e943e2ec
alli-Codes <allisonevans081@gmail.com>
2022-11-17 10:47:43 +0100

    Creating and importing the Sidebar component

--- src/components/Default.vue
+++ src/components/Default.vue
@@ -1,14 +1,17 @@
 <script>
 import Header from "./Header.vue";
+import Sidebar from "./Sidebar.vue";
 
 export default {
   name: "Default",
   components: {
     Header,
+    Sidebar,
   },
 };
 </script>
 
 <template>
   <Header />
+  <Sidebar />
 </template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,5 @@
+<script></script>
+
+<template>
+  <div class="h-full w-[20rem] bg-red-400"></div>
+</template>
