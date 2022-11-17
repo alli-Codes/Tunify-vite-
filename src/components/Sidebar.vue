58d5d6b8813cf8821accbf96ffe7f3414e1cd12e
alli-Codes <allisonevans081@gmail.com>
2022-11-17 12:59:02 +0100

    Adding, creating, importing all new icon components in Sidebar component

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,26 +1,51 @@
-<script></script>
+<script>
+import HomeIcon from "./icons/HomeIcon.vue";
+import ExploreIcon from "./icons/ExploreIcon.vue";
+import SongsIcon from "./icons/SongsIcon.vue";
+import ArtistIcon from "./icons/ArtistIcon.vue";
+import AlbumIcon from "./icons/AlbumIcon.vue";
+import RadioIcon from "./icons/RadioIcon.vue";
+import RecentIcon from "./icons/RecentIcon.vue";
+import FavoriteIcon from "./icons/FavoriteIcon.vue";
+import PlaylistIcon from "./icons/PlaylistIcon.vue";
+
+export default {
+  name: "Sidebar",
+  components: {
+    HomeIcon,
+    ExploreIcon,
+    SongsIcon,
+    ArtistIcon,
+    AlbumIcon,
+    RadioIcon,
+    RecentIcon,
+    FavoriteIcon,
+    PlaylistIcon,
+  },
+};
+</script>
 
 <template>
   <div class="h-full w-[20rem] bg-[#FFF8F8] absolute top-0">
     <div class="bg-red-40 font-body flex flex-col relative top-20">
       <section class="pt-10 flex flex-col gap-10">
-        <p class="px-5 text-[#C4C4C4]">BROWSER</p>
-        <nav class="bg-green-40 flex flex-col gap-4">
-          <a href="#">Home</a>
-          <a href="#" class="active">Explore</a>
-          <a href="#">Songs</a>
-          <a href="#">Artists</a>
-          <a href="#">Albums</a>
-          <a href="#">Radio</a>
+        <p class="px-5 text-[#C4C4C4] text-[10px]">BROWSER</p>
+        <nav class="bg-green-40 flex flex-col gap-4 text-xs">
+          <a href="#"><HomeIcon class="w-4" />Home</a>
+          <a href="#" class="active"><ExploreIcon class="w-4" />Explore</a>
+          <a href="#"><SongsIcon class="w-4" /> Songs</a>
+          <a href="#"><ArtistIcon class="w-4" />Artists</a>
+          <a href="#"><AlbumIcon class="w-4" />Albums</a>
+          <a href="#"><RadioIcon class="w-4" />Radio</a>
         </nav>
       </section>
 
       <section class="pt-10 flex flex-col gap-10">
-        <p class="px-5 text-[#C4C4C4]">YOUR LIBRARY</p>
-        <nav class="bg-green-40 flex flex-col gap-4">
-          <a href="#">Recently Played</a>
-          <a href="#">Favourites</a>
-          <a href="#">Playlists</a>
+        <p class="px-5 text-[#C4C4C4] text-[10px]">YOUR LIBRARY</p>
+        <nav class="bg-green-40 flex flex-col gap-4 text-xs">
+          <a href="#"><RecentIcon class="w-4" />Recently Played</a>
+          <a href="#"><FavoriteIcon class="w-4" />Favourites</a>
+          <a href="#"><PlaylistIcon class="w-4" />Playlists</a>
         </nav>
       </section>
     </div>
